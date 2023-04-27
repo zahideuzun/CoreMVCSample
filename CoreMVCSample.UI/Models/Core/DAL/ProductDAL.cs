8023727664f544d7a278c9df6fb854c1f48acee9
zahide <108192527+zahideuzun@users.noreply.github.com>
2023-04-27 21:25:25 +0300

    v3

--- CoreMVCSample.UI/Models/Core/DAL/ProductDAL.cs
+++ CoreMVCSample.UI/Models/Core/DAL/ProductDAL.cs
@@ -1,7 +1,26 @@
-﻿namespace CoreMVCSample.UI.Models.Core.DAL
+﻿using System.Collections.Generic;
+using System.Linq;
+using CoreMVCSample.UI.Models.Core.Context;
+using CoreMVCSample.UI.Models.Core.Entities;
+
+namespace CoreMVCSample.UI.Models.Core.DAL
 {
 	public class ProductDAL
 	{
+		private readonly ProductAllergyContext _context;
+		ProductDAL(ProductAllergyContext context)
+		{
+			_context = context;
+		}
 
+		public List<Product> GetAllProducts()
+		{
+			return _context.Products.ToList();
+		}
+
+		public void AddProduct(Product product)
+		{
+
+		}
 	}
 }
