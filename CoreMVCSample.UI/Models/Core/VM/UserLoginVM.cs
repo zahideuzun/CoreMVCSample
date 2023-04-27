3b70b3cd6926e36ae3557d8269ac9a5d41dbbfc0
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-04-27 21:32:25 +0300

    userloginvm eklendi

--- CoreMVCSample.UI/Models/Core/VM/UserLoginVM.cs
+++ CoreMVCSample.UI/Models/Core/VM/UserLoginVM.cs
@@ -1,6 +1,10 @@
-﻿namespace CoreMVCSample.UI.Models.Core.VM
+﻿using System.Security.AccessControl;
+
+namespace CoreMVCSample.UI.Models.Core.VM
 {
 	public class UserLoginVM
 	{
+		public string Email { get; set; }
+		public string Password { get; set; }
 	}
 }
